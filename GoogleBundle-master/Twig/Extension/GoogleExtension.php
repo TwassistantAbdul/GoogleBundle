8cb62dcc99549857affcc690f503188276c32baa
TwassistantAbdul <abdul.g@twassistant.com>
2013-12-04 22:54:49 +0530

    adding googlebundle
    adding googlebundle

---
+++ GoogleBundle-master/Twig/Extension/GoogleExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the BITGoogleBundle package. (c) bitgandtter <http://bitgandtter.github.com/> For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+namespace BIT\GoogleBundle\Twig\Extension;
+
+use Symfony\Component\DependencyInjection\Container;
+
+class GoogleExtension extends \Twig_Extension
+{
+
+  protected $container;
+
+  public function __construct( Container $container )
+  {
+    $this->container = $container;
+  }
+
+  public function getFunctions( )
+  {
+    $extra = array ( 'is_safe' => array ( 'html' ) );
+    
+    $functions = array ();
+    $functions [ 'google_login_button' ] = new \Twig_Function_Method ( $this, 'renderLoginButton', $extra );
+    $functions [ 'google_login_url' ] = new \Twig_Function_Method ( $this, 'renderLoginUrl', $extra );
+    return $functions;
+  }
+
+  private function helper( )
+  {
+    return $this->container->get ( 'bit_google.helper' );
+  }
+
+  public function renderLoginButton( )
+  {
+    return $this->helper ( )->loginButton ( );
+  }
+
+  public function renderLoginUrl( )
+  {
+    return $this->helper ( )->loginUrl ( );
+  }
+
+  public function getName( )
+  {
+    return 'google';
+  }
+
+}
